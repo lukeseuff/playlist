2447f686415b967bd31dcd5c310c0159fac73cbb
Luke Seuffert <lukeseffert@gmail.com>
2019-02-21 12:03:49 -0500

    server login

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 const express = require('express')
 const next = require('next')
 const playlist = require('./routes/playlist')
+const admin = require('firebase-admin')
 
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const request = require('./lib/client/request')
+const firebase = admin.initializeApp(
+  {
+    credential: admin.credential.cert(require('./config/firebase/server')),
+    databaseURL: 'https://playlist-36d9e.firebaseio.com'
+  },
+  'server'
+)
 
 app.prepare().then(() => {
   const server = express()
 
+  server.use((req, res, next) => {
+    req.firebaseServer = firebase
+    next()
+  })
+
   server.use('/api/playlist', playlist)
+
+  server.post('/api/login', (req, res) => {
+    if (!req.body) return res.sendStatus(400)
+
+    const token = req.body.token
+    firebase.auth()
+            .verifyIdToken(token)
+            .then(decodedToken => {
+              req.session.decodedToken = decodedToken
+              return decodedToken
+            })
+            .then(decodedToken => res.json({ status: true, decodedToken }))
+            .catch(error => res.json({ error }))
+  })
 
   server.get('*', (req, res) => {
     return handle(req, res)
